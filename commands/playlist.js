9d7f2790e603a99acc7098e64fc728c0b60ac014
TKSds <tks.dsm@gmail.com>
2022-09-09 11:59:25 +0300

    SBOT - Added playlist command

---
+++ commands/playlist.js
@@ -0,0 +1,43 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const fs = require("fs");
+
+// constants used to represent folder names
+const OPTION_NERVI = "nervi";
+const OPTION_ALCOOLIC = "alcoolic";
+const OPTION_ANEVRISM = "anevrism";
+
+// list that holds all audio files
+var list = [];
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("playlist")
+    .setDescription("Displays all possible play commands for swk bot 2.0!"),
+  async execute(interaction) {
+    populateListFromLocalAudioFiles(list, OPTION_NERVI);
+    populateListFromLocalAudioFiles(list, OPTION_ALCOOLIC);
+    populateListFromLocalAudioFiles(list, OPTION_ANEVRISM);
+    console.log(list);
+    await interaction.channel.send(
+      list.map((i) => `${list.indexOf(i) + 1}. ${i}`).join("\n")
+    );
+  },
+};
+
+/**
+ * Method populates list that is used to display possbile play options.
+ *
+ * @param {*} list to hold options
+ * @param {*} option where the audio files are located
+ */
+function populateListFromLocalAudioFiles(list, option) {
+  const audioFiles = fs
+    .readdirSync(`./resources/audioFiles/${option}`)
+    .filter((file) => file.endsWith(".mp3"));
+
+  for (const file of audioFiles) {
+    var fileNameArray = file.split(".");
+    const fileName = fileNameArray[0];
+    list.push(fileName);
+  }
+}
